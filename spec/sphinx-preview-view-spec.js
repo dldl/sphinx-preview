5707be6917f718c4512d34865ad7a27657e434ae
Coac <levictor3000@hotmail.fr>
2017-01-15 17:26:18 +0100

    Removed unused generated stuff

--- spec/sphinx-preview-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import SphinxPreviewView from '../lib/sphinx-preview-view';
-
-describe('SphinxPreviewView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
